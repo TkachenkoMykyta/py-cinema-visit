120d031888c7f96b34a0285cdafef50ee60075c9
Medaka <11Draghost11@gmail.com>
2026-03-03 11:15:43 +0200

    changes in bar.py

--- app/main.py
+++ app/main.py
@@ -4,11 +4,12 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list,
-                 hall_number: int,
-                 cleaner: str,
-                 movie: str
-                 ) -> None:
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
     customers_list = customers
     clean_staff = Cleaner(name=cleaner)
     hall = CinemaHall(number=hall_number)
